941c3a55dd87bbcbe73fd6d6d413c4c523ca8df6
oliveruhlar <oliver.uhlar@pantheon.tech>
2021-01-12 17:41:51 +0100

    100pingov

--- docker_app.py
+++ docker_app.py
@@ -12,16 +12,9 @@ def get_con_ip(con_id):
                            universal_newlines=True)
     return(process.stdout.readline()[:-1])
 
-def print_ping(con1,con2_ip):
-    p = Popen(['docker', 'exec', con1, 'ping','-c', '3', con2_ip], 
-            stdout=PIPE,
-            stderr=PIPE,
-            shell=False)
-    p.wait()
-    return(p.stdout.read().decode())
 
-def print_ping2(con1,con2_ip):
-    p = Popen(['docker', 'exec', con1, 'ping','-c', '3', con2_ip], 
+def print_ping(con1,con2_ip):
+    p = Popen(['docker', 'exec', con1, 'ping','-c', '100', con2_ip], 
                         stdout=PIPE,
                         shell=True)
     return p.communicate()[0]
@@ -35,29 +28,13 @@ run("docker ps")
 
 con1_ip = get_con_ip(con1)
 con2_ip = get_con_ip(con2)
-""" 
+
 print('\n')
-print("con1: ping -c 3 con2",'\n')
+print("con1: ping -c 100 con2",'\n')
 print(print_ping(con1,con2_ip))
 print('\n')
-print("con1: ping -c 3 con2",'\n')
-print(print_ping(con2,con1_ip)) """
-
-
-
-print('\n')
-print("con1: ping -c 3 con2",'\n')
-print(print_ping2(con1,con2_ip))
-print('\n')
-print("con1: ping -c 3 con2",'\n')
-print(print_ping2(con2,con1_ip))
-
-
-
-""" result = sys("docker exec -it "+ con1 + " ping -c 3 " + con2_ip)
-print('\n')
-result = sys("docker exec -it "+ con2 + " ping -c 3 " + con1_ip)
-print(str(result.stdout)) """
+print("con1: ping -c 100 con2",'\n')
+print(print_ping(con2,con1_ip))
 
 run("docker stop "+con1)
 run("docker stop "+con2)
